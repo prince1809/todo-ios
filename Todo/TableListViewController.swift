6fbacb86497625d2aa814e31da7e2b99c22953c7
prince <prince18pat@gmail.com>
2019-09-06 00:33:53 +0900

    add an item to todo

--- Todo/TableListViewController.swift
+++ Todo/TableListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TableListViewController: UITableViewController {
 
-    let itemArray = ["Go out", "Buy beer", "Eat Pizza"]
+    var itemArray = ["Go out", "Buy beer", "Eat Pizza"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,6 +30,36 @@ class TableListViewController: UITableViewController {
         
         return cell
     }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 
-
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Todo Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
